e1df4cc973144620b97748e187166e5405507d4b
sasuuu <fmerta@gmail.com>
2023-03-14 23:01:50 +0100

    Add SongsDownloader class for child process for downloading songs

--- MusicBot/music_bot.py
+++ MusicBot/music_bot.py
@@ -1,10 +1,25 @@
 from discord.ext import commands
+from multiprocessing import Queue, Lock
+from SongsDownloader.songs_downloader import *
 
 class MusicBot(commands.Bot):
 
     def __init__(self, prefix, guild_id):
         super().__init__(command_prefix=prefix)
         self.guild_id = int(guild_id)
+        self.songs_to_download = Queue()
+        self.songs_to_download_lock = Lock()
+        self.downloaded_songs = Queue()
+        self.downloaded_songs_lock = Lock()
+        self.SongsDownloaderProcess = SongsDownloader(self.songs_to_download, self.songs_to_download_lock, self.downloaded_songs, self.downloaded_songs_lock)
+        print(f'Starting SongsDownloader Process')
+        self.SongsDownloaderProcess.start()
+        print(f'SongsDownloader Process started(PID: {self.SongsDownloaderProcess.pid})')
 
     def finalize(self):
-        print("closing bot")
+        print(f'Stopping SongsDownloader Process(PID: {self.SongsDownloaderProcess.pid})')
+        self.SongsDownloaderProcess.stop_event.set()
+        print(f'Waiting for SongsDownloader Process(PID: {self.SongsDownloaderProcess.pid}) exit')
+        self.SongsDownloaderProcess.join()
+        print(f'SongsDownloader Process exited with code {self.SongsDownloaderProcess.exitcode}')
+        
--- main.py
+++ main.py
@@ -8,23 +8,22 @@ discord_token = configuration["discord_token"]
 discord_guild_id = configuration["discord_guild_id"]
 discord_command_prefix = "!"
 
-bot = MusicBot(discord_command_prefix, discord_guild_id)
-
-@bot.command(name='play')
-async def play(ctx, *, message):
-    if ctx.guild.id != ctx.bot.guild_id:
-        print(f'Skipping because guild_id({ctx.guild.id}) is different than {ctx.bot.guild_id}')
-        return
-
-    await ctx.send(f'Responding to message({message}) in guild with id: {ctx.guild.id}')
-
-@bot.event
-async def on_ready():
-    print("Logged in as a bot {0.user}".format(bot))
-
 if __name__ == "__main__":
     print(f'Discord token: {discord_token}')
     print(f'Discord guild: {discord_guild_id}')
+    bot = MusicBot(discord_command_prefix, discord_guild_id)
+
+    @bot.command(name='play')
+    async def play(ctx, *, message):
+        if ctx.guild.id != ctx.bot.guild_id:
+            print(f'Skipping because guild_id({ctx.guild.id}) is different than {ctx.bot.guild_id}')
+            return
+
+        await ctx.send(f'Responding to message({message}) in guild with id: {ctx.guild.id}')
+
+    @bot.event
+    async def on_ready():
+        print("Logged in as a bot {0.user}".format(bot))
 
     bot.run(discord_token)
     bot.finalize()
